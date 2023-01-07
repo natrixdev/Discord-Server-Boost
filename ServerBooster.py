7888de656c0e7fa58640d27158caeec053e55e3f
natrix_dev <discordlenko@gmail.com>
2023-01-07 12:36:02 +0100

    Create ServerBooster.py

---
+++ ServerBooster.py
@@ -0,0 +1,27 @@
+import os; import discord; import time; import colorama; import captchaSolver
+
+import req # module for requests (used at line 12).
+
+config = "/config/server.json"
+message = "/config/boost.json"
+
+from config import serverID, ownerID
+from message import BoostMessageChannel, BoostMessage
+
+
+try: config;
+try modules;
+print("Modules & Config are working :")
+print("Your server is starting to be boosted")
+print("github.com/natrixdev")
+
+def __boostBotter__:
+  req.url="https://discord.app/dev/uid={ownerID}&method=boost{serverID}"
+  boost = req.succes 
+  while boost=True:
+    BoostMessageChannel.send{BoostMessage}
+  if boost > 30: 
+    return:
+    print("You reached 30 boost, please stop the program and run it later or your server can be banned from Discord.")
+    
+ boost=boostBotter()
